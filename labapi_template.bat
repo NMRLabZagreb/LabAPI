c351e5fb6e59ebb7e147ca26c08b1094c8037d96
NMR Lab <nmrlab@phy.hr>
2024-12-17 14:01:54 +0100

    Adding Windows startup template .bat

---
+++ labapi_template.bat
@@ -0,0 +1,14 @@
+@echo off
+REM Change the following paths
+REM Activate Python environment
+call "D:/path-to-LabAPI/.venv/Scripts/activate.bat"
+
+REM Change working directory to LabAPI root folder and run the Waitress server
+cd /d D:/path-to-LabAPI/src/
+start pythonw run_server.py
+
+REM Change working directory to nginx and run it
+cd /d C:/path-to-nginx/
+start nginx.exe
+REM Close cmd window
+exit
