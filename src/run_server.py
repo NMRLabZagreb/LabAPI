2cb16d242e34fb9f1cb7278d4ec21ea97676b6a4
ivanjkv <54992169+ivanjkv@users.noreply.github.com>
2025-03-13 14:30:54 +0100

    Simple Prometheus exporter for LakeShore336

--- src/run_server.py
+++ src/run_server.py
@@ -1,8 +1,12 @@
 from waitress import serve
+from prometheus_client import start_http_server
 from flaskr import create_app
 
 # Create Flask app
 app = create_app()
+
+# Serve Prometheus metrics
+start_http_server(2025)
 
 # Serve it on localhost:5000
 serve(app, host='127.0.0.1', port='5000', threads=1)
